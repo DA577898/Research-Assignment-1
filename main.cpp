453fc43d0c07209dd9356773eb9b4f8c85a9bd36
Daniel <dhernandez8@ufl.edu>
2024-10-31 04:40:08 -0400

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -34,6 +34,8 @@ void threadFunction(int value, int startingIndex, int endingIndex, vector<bool> 
 
 /**
  * This function is called to invoke the threads to calculate the prime numbers.
+ * It works by splitting up the workload equally among the threads, performing the sieve
+ * on different chunks of the array simultaneously.
  * 
  * @param numPrimes
  * @param numThreads
@@ -43,18 +45,19 @@ void threadFunction(int value, int startingIndex, int endingIndex, vector<bool> 
 
 void invokeThreads(int numPrimes, int numThreads, vector<bool> &values) {
     vector<thread> threads;
-    int runs = sqrt(numPrimes);
+    int runs = ceil(sqrt(numPrimes));
 
     for(int i = 2; i < runs; i++) {
         if(values[i]) {
             int start = i * i;
             int end = numPrimes;
             int range = end - start;
-            int workload = ceil((double)range / (double)numThreads);
+            int workload = range / numThreads;
 
             int lastStartingValue = start;
 
-            for(int j = 0; j < numThreads; j++) {                
+            for(int j = 0; j < numThreads; j++) {
+                // Split workload:        
                 int endValue = ((lastStartingValue + workload) / i )* i;
                 threads.push_back(thread(threadFunction, i, lastStartingValue, endValue, ref(values)));
                 lastStartingValue = endValue;
@@ -71,10 +74,11 @@ void invokeThreads(int numPrimes, int numThreads, vector<bool> &values) {
 vector<int> findPrimeValues(int numPrimes, int threads) {
     vector<bool> values(numPrimes, true);
     vector<int> primes;
-    values[0] = false; values[1] = false;
+    values[0] = false; values[1] = false;  // Initialize 0 and 1 as non primes.
 
     invokeThreads(numPrimes, 8, values);
 
+    // Add the prime numbers to the vector.
     for(int i = 0; i < numPrimes; i++) {
         if(values.at(i)) {
             primes.push_back(i);
@@ -84,11 +88,11 @@ vector<int> findPrimeValues(int numPrimes, int threads) {
 }
 
 int main(int argc, char* argv[]) {
-    int numPrimes = 10000;
+    int numPrimes = 100000000;
     int numThreads = 8;
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-    ofstream myFile("file.txt");
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now(); // Timer
     
+    ofstream myFile("file.txt");
     vector<int> primes = findPrimeValues(numPrimes, numThreads);
     myFile << "Count: " << primes.size() << endl;
     myFile << "Time: " << chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - begin).count() << " ms" << endl;
