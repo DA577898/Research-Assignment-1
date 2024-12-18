f1476bbad43a2570abd24ffe326d17ff51e9aad4
Daniel <dhernandez8@ufl.edu>
2024-12-18 00:59:48 -0500

    finally implemented multithreaded wheel factorization

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 using namespace std;
 
 const int MAX_THREADS = 8;
-const int MAX_PRIME = 100;
+const int MAX_PRIME = 100000000;
+BS::thread_pool THREAD_POOL(MAX_THREADS);
+
 
 /**
  * This program calculates the prime numbers up to a given number using the Sieve of Eratosthenes algorithm.
@@ -21,7 +23,6 @@ const int MAX_PRIME = 100;
 void sieveValue(vector<bool> &primes, int i){
     int value = (i*2)+3;
     for(int j = value * value; j < MAX_PRIME; j += (value * 2)){ // Skip even numbers
-        cout << j << ", Index to be removed " << (j-3)/2 << endl;
         primes[(j-3)/2] = false;
     }
 }
@@ -35,14 +36,44 @@ void sieveVector(vector<bool> &primes){
     }
 }
 
+void individualWheelValue(vector<bool> &primes, int value){
+    for(int j = value + 30; j < MAX_PRIME; j += 30){ // Every 30 mark the next possible prime following the wheel pattern since it's a mod 30 wheel.
+        primes[(j-3)/2] = true;
+    }
+}
+
+void wheelFactorization(vector<bool> &primes){
+    // First, set primes from 0 to thirty to true.
+    vector<int> primesToThirty = {3, 5, 7, 11, 13, 17, 19, 23, 29};  // Set first primes excluding 2 since it will be added at the final calculation. 
+    for(auto i : primesToThirty){
+        primes[(i-3)/2] = true;
+    }
+
+    // Then using the wheel create a list of potential primes (up to sqrt of max prime since it will be iterated until that number).
+    // The numbers are obtained by removing all multiples of 2, 3, 5, leaving us with 8 numbers ( = to thread count).
+    // Since 2*3*5 = 30, the values will repeat every 30 numbers to form the pattern. (mod 30)
+    vector<int> wheel = {1, 7, 11, 13, 17, 19, 23, 29};  // Size of wheel is 8, since we have 8 threads.
+
+    int chunks = sqrt(MAX_PRIME) / 30;  // Number of chunks to divide the wheel into.
+
+    for(int i = 0; i < MAX_THREADS; i++){
+        THREAD_POOL.detach_task([=, &primes] {
+            individualWheelValue(ref(primes), wheel[i]);
+        });
+        THREAD_POOL.wait();
+    }
+}
 
 int main(int argc, char** argv){
-    vector<bool> primes((MAX_PRIME/2 - 1), true);  // (3, end] inclusive.
+    vector<bool> primes((MAX_PRIME/2 - 1), false);  // (3, end] inclusive.
+    wheelFactorization(primes);
     sieveVector(primes);
 
+    ofstream file("file.txt");
     for(int i = 0; i < primes.size(); i++){
         if(primes[i]){
-            cout << i*2+3 << " ";
+            file << (i*2)+3 << ", ";
         }
     }
+    file << endl << "Total primes: " << count(primes.begin(), primes.end(), true) + 1;  // Add 2 to the count.
 }
