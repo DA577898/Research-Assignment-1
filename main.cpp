bb2357acac9b96b52dd989a40b80e80becb96fcf
Daniel Hernandez <da577898@ucf.edu>
2025-01-03 14:29:31 -0500

    change to bool vector for future usage

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@
 #include <fstream>
 #include <chrono>
 #include "BS_thread_pool.hpp"
+#include <future>
 
 using namespace std;
 
 const int MAX_THREADS = 8;
-const int MAX_PRIME = 100000000;
+const int MAX_PRIME = 10000;
 BS::thread_pool THREAD_POOL(MAX_THREADS);
 
 
@@ -22,108 +23,167 @@ BS::thread_pool THREAD_POOL(MAX_THREADS);
 
 
 
+
+
+/*void sieveValue(vector<bool> &primes, int i){
+    int value = (i*2)+3;
+    for(int j = value; j < sqrt(MAX_PRIME); j += (value * 2)){ // Skip even numbers
+        primes[(j-3)/2] = false;
+    }
+}*/
+
+void threadFunction(int value, int startingIndex, int endingIndex, vector<bool> &values) {
+    vector<int> sequence = {1, 7, 11, 13, 17, 19, 23, 29};
+    
+    for (int i = startingIndex; i < endingIndex; i += value * 2) {
+        values[i] = false;
+    }
+}
+
+void invokeThreads(int numPrimes, int numThreads, vector<bool> &values) {
+    int runs = ceil(sqrt(numPrimes));
+
+    for (int i = 3; i < runs; i += 2) {  // Go only through odd numbers (evens are all non-prime and code below accounts for it)
+        if (values[i]) {
+            int start = i * i;
+            int end = numPrimes;
+            int range = end - start;
+            int workload = range / numThreads;
+
+            for (int j = 0; j < numThreads; j++) {
+                int lastStartingValue = start + j * workload;
+                int endValue = min(start + (j + 1) * workload, end);
+                THREAD_POOL.detach_task([=, &values] {
+                    threadFunction(i, lastStartingValue, endValue, ref(values));
+                });
+            }
+        }
+    }
+    THREAD_POOL.wait();
+}
+
 void sieveValue(vector<bool> &primes, int i){
     int value = (i*2)+3;
-    for(int j = value * value; j < sqrt(MAX_PRIME); j += (value * 2)){ // Skip even numbers
+    for(int j = value * value; j < MAX_PRIME; j += (value * 2)){ // Skip even numbers
         primes[(j-3)/2] = false;
     }
 }
 
-void sieveVector(vector<bool> &primes){
-    
-    int limit = sqrt(MAX_PRIME) / 2;  // Avoid recalculating every run
-    for(int i = 0; i < limit; i++){
+void sieveVector(vector<vector<bool>> &wheel){
+    vector<bool> primes = {wheel[0].begin(), wheel[0].begin() + sqrt(MAX_PRIME) / 2 - 1};
+/*     for(int i = 0; i < primes.size(); i++){
         if(primes[i]){
-            THREAD_POOL.detach_task([=, &primes] {
-                sieveValue(primes, i);
-            });
-            THREAD_POOL.wait();
+            cout << "Sieve value: " << 2 * i + 3 << endl;
+        }
+    } */
+/*
+    primes[0] = true;  // 3 is prime.
+    primes[1] = true;  // 5 is prime.
+
+    for(auto value : wheel[0]){
+        if(value > sqrt(MAX_PRIME)){break;}
+        primes[(value-3)/2] = true;
+    }
+
+    invokeThreads(10000, MAX_THREADS, primes);
+
+    int count = 1;  // 2 is prime.
+    for (bool prime : primes) {
+        if (prime) {
+            count++;
         }
     }
-}
+    cout << "\nTotal primes: " << count << endl;
+    
 
-/*void segmentedSieve(int value, int startingIndex, int endingIndex, vector<bool> &primes) {
-    endingIndex = min(endingIndex, MAX_PRIME);
-    if(startingIndex % 2 == 0){ 
-        printf("Odd found: %d, the value is %d\n", startingIndex, value);
-        startingIndex -= value;
-    }
-    for(int i = startingIndex; i <= endingIndex; i+=(value*2)) { // * 2 so it iterates through odd numbers only
-        primes[(i-3)/2] = false;
-    }
-}
+    
+        vector<future<vector<int>>> futures;
+    futures.reserve(MAX_THREADS);
 
-void sieveVector(vector<bool> &primes){
-    int runs = ceil(sqrt(MAX_PRIME));
+    vector<bool> primes((MAX_PRIME/2) - 1, false);
+    primes[0] = true;  // 3 is prime.
+    primes[1] = true;  // 5 is prime.
 
-    for(int i = 0; i < runs; i++) {
-        if(primes[i]) {
-            int value = (i*2)+3;
-            int start = value * value;
-            int end = MAX_PRIME;
-            int range = end - start;
-            int workload = range / MAX_THREADS;
-
-            int lastStartingValue = start;
-
-            for(int j = 0; j < MAX_THREADS; j++) {
-                // Split workload:        
-                int endValue = (lastStartingValue + workload);
-                THREAD_POOL.detach_task([=, &primes] {  
-                    segmentedSieve(value, lastStartingValue, endValue, ref(primes));
-                });
-                lastStartingValue = endValue;
-            }
+    for (auto vec : wheel) {
+        for (int num : vec) {
+            primes[(num/2) - 1] = true;
         }
-        THREAD_POOL.wait();
     }
-}*/
 
-void individualWheelValue(int startValue, int endValue){
+    for(int i = 0; i < primes.size(); i++){
+        if(primes[i]){
+            sieveValue(primes, i);
+        }
+    }
+
+    int count = 1;  // 2 is prime.
+    for (bool prime : primes) {
+        if (prime) {
+            count++;
+        }
+    }
+    cout << "\nTotal primes: " << count;
+    cout << endl;*/
+}
+    
+
+
+
+vector<bool> individualWheelValue(int startValue, int endValue, int chunkNumber){
     vector<int> offsets = {4, 2, 4,  2,  4,  6,  2,  6};
     unordered_map<int, int> wheelLookup = {{1, 0}, {7, 1}, {11, 2}, {13, 3}, {17, 4}, {19, 5}, {23, 6}, {29, 7}}; // to get the index of the offset
-    vector<int> wheel;
+    vector<bool> wheel((endValue - startValue)/2 + 10, false);
 
     int value = startValue;
-    while (wheelLookup.count(value % 30) == 0) { value++; }
+    while (wheelLookup.count(value % 30) == 0) { value++; }  // find next value that will be part of the wheel
     int index = wheelLookup[value % 30] + 7;
-    while(value < endValue){
-        cout << value % 30 << ", ";
-        wheel.push_back(value);
+    while(value < endValue){  // calculate the wheel values
+        wheel[((value % (MAX_PRIME / MAX_THREADS)) - 3) / 2] = true;
         value += offsets[index % 8];
         index++;
     }
+    return wheel;
 }
 
-void wheelFactorization(){
+void wheelFactorization(vector<vector<bool>> &wheel){
+    vector<future<vector<bool>>> futures;
+    futures.reserve(MAX_THREADS);
+    wheel.reserve(MAX_THREADS);
 
-    individualWheelValue(0, 1000);
-    individualWheelValue(1000, 2000);
+    for(int i = 0; i < 8; i++){
+        int start = (i == 0) ? 7 : i * (MAX_PRIME / MAX_THREADS);
+        int end = min((i + 1) * (MAX_PRIME / MAX_THREADS), MAX_PRIME);
+        futures.push_back(THREAD_POOL.submit_task([=] () {
+            return individualWheelValue(start, end, i);
+        }));
+    }
+    THREAD_POOL.wait();
 
-
+    for(auto &future : futures){
+        wheel.push_back(future.get());
+    }
+    wheel[0][0] = true;  // 3 is prime.
+    wheel[0][1] = true;  // 5 is prime;
+    for (int i = 0; i < wheel.size(); i++) {
+        for (int j = 0; j < wheel[i].size(); j++) {
+            if (wheel[i][j]) {
+                cout << 2 * (i * (MAX_PRIME / MAX_THREADS) + j) + 3 << " ";
+            }
+        }
+    }
+    cout << endl;
 }
 
 void run(vector<long long> &runTimes){
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now(); // Starting time
+    vector<vector<bool>> wheel;
+    auto begin = chrono::steady_clock::now(); // Starting time
 
-    wheelFactorization();/*
+    wheelFactorization(wheel);
     sieveVector(wheel);
 
-    long long time = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - begin).count(); // Ending time
-    cout << "runtime: " << time << " ms" << endl;
-    runTimes.push_back(time);
 
-    ofstream file("file.txt");
-
-    int count = 1;  // 2 is prime.
-    for(int i = 0; i < primes.size(); i++){
-        if(primes[i]){
-            count++;
-
-        }
-    }
-    file << endl << "Total primes: " << count << endl;*/
-    
+    auto time = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - begin).count(); // Ending time
+    runTimes.push_back(time);    
 }
 
 int main(int argc, char** argv){
